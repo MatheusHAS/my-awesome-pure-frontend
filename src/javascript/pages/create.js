1ac1e292024ad548db81b2d473f40c5df0d3bc78
Matheus Azambuja <matheushenrique.ads@gmail.com>
2021-01-15 21:01:27 -0300

    feat: improvements on textfield and buttons

--- src/javascript/pages/create.js
+++ src/javascript/pages/create.js
@@ -1,6 +1,9 @@
 import { Validator } from '@/javascript/modules'
+import Textfield from '@/styles/components/src/textfield/textfield'
+
 const createPage = () => {
   const validator = new Validator()
+  Textfield.mount()
 }
 
 export default createPage
--- src/styles/components/src/textfield/textfield.js
+++ src/styles/components/src/textfield/textfield.js
@@ -1,56 +1,51 @@
 const ClassMap = {
-  component: ".js-float-field",
-  input: ".js-float-input:not(.is-upgraded)",
-};
+  component: '.js-float-field',
+  input: '.js-float-input:not(.is-upgraded)',
+}
 
-const Typings = ["tel", "numeric"];
+const Typings = ['tel', 'numeric']
 
 const mount = () => {
-  const inputs = document.querySelectorAll(ClassMap.input);
+  const inputs = document.querySelectorAll(ClassMap.input)
+  console.log(inputs)
 
   if (inputs.length > 0) {
-    inputs.forEach((input) => upgrade(input));
+    inputs.forEach((input) => upgrade(input))
   }
 
-  document.addEventListener("input", ({ target }) => {
-    if (target.tagName.toLowerCase() === "input") {
-      delegateEvents(target);
+  document.addEventListener('input', ({ target }) => {
+    if (target.tagName.toLowerCase() === 'input') {
+      delegateEvents(target)
     }
-  });
+  })
 
-  document.addEventListener("change", ({ target }) => {
-    if (target.tagName.toLowerCase() === "select") {
-      delegateEvents(target);
+  document.addEventListener('change', ({ target }) => {
+    if (target.tagName.toLowerCase() === 'select') {
+      delegateEvents(target)
     }
-  });
-};
+  })
+}
 
 const delegateEvents = (target) => {
   if (target.closest(ClassMap.component)) {
-    target
-      .closest(ClassMap.component)
-      .classList.toggle("is-dirty", dirtyInput(target));
+    target.closest(ClassMap.component).classList.toggle('is-dirty', dirtyInput(target))
   }
-};
+}
 
 const upgrade = (input) => {
-  input.classList.add("is-upgraded");
-
-  input
-    .closest(ClassMap.component)
-    .classList.toggle("is-dirty", dirtyInput(input));
-
-  changeType(input);
-};
+  input.classList.add('is-upgraded')
+  input.closest(ClassMap.component).classList.toggle('is-dirty', dirtyInput(input))
+  changeType(input)
+}
 
 const changeType = (input) => {
-  const type = input.getAttribute("data-typing");
+  const type = input.getAttribute('data-typing')
 
   if (Typings.includes(type)) {
-    input.type = type;
+    input.type = type
   }
-};
+}
 
-const dirtyInput = (input) => input.value.trim().length > 0;
+const dirtyInput = (input) => input?.value?.trim()?.length > 0
 
-export default { mount, upgrade };
+export default { mount, upgrade }
