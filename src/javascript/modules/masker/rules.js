9aea3fac4230af507585df242d59be40a38d3b66
Matheus Azambuja <matheushenrique.ads@gmail.com>
2021-01-17 02:30:31 -0300

    refactor: masker

--- src/javascript/modules/masker/rules.js
+++ src/javascript/modules/masker/rules.js
@@ -1,9 +1,12 @@
+const DEFAULT_KEYS_IGNORE = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'Backspace', 'Delete', 'Tab']
+const DEFAULT_KEYS_ALLOWED_NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
 export default {
   cpf: {
     mask: '000.000.000-00',
     event: (event) => {
       let currentValue = event.target.value
-      if (['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].includes(event.key)) {
+      if (DEFAULT_KEYS_ALLOWED_NUMBERS.includes(event.key)) {
         if (![46, 8].includes(event.keyCode)) {
           if ([3, 7].includes(currentValue.length)) {
             event.target.value = `${currentValue}.`
@@ -11,9 +14,7 @@ export default {
             event.target.value = `${currentValue}-`
           }
         }
-      } else if (
-        !['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'Backspace', 'Delete', 'Tab'].includes(event.key)
-      ) {
+      } else if (!DEFAULT_KEYS_IGNORE.includes(event.key)) {
         event.preventDefault()
       }
     },
@@ -21,13 +22,17 @@ export default {
   phone: {
     mask: '(00) 99999-9999',
     event: (event) => {
-      const { selectionStart, value } = event.target
-      if (selectionStart === 0) {
-        event.target.value = `(${value}`
-      } else if (selectionStart === 3) {
-        event.target.value = `${value}) `
-      } else if (selectionStart === 10) {
-        event.target.value = `${value}-`
+      if (DEFAULT_KEYS_ALLOWED_NUMBERS.includes(event.key)) {
+        const { selectionStart, value } = event.target
+        if (selectionStart === 0) {
+          event.target.value = `(${value}`
+        } else if (selectionStart === 3) {
+          event.target.value = `${value}) `
+        } else if (selectionStart === 10) {
+          event.target.value = `${value}-`
+        }
+      } else if (!DEFAULT_KEYS_IGNORE.includes(event.key)) {
+        event.preventDefault()
       }
     },
   },
