ee964b822f29bb7ea54f9db89b5898b82b09d136
Matheus Azambuja <matheushenrique.ads@gmail.com>
2021-01-17 03:16:50 -0300

    refactor(modules): remove unnecessary modules

--- src/javascript/modules/index.js
+++ src/javascript/modules/index.js
@@ -1,10 +1,8 @@
 import { ready } from './ready'
-import Request from './request'
-import { debounce } from './debounce'
 import { Validator } from './validator/validator'
 import { Form } from './form'
 import { LocalStorage } from './localStorage'
 import { Router } from './router'
 import { Masker } from './masker/masker'
 
-export { ready, Request, debounce, Validator, Form, LocalStorage, Router, Masker }
+export { ready, Validator, Form, LocalStorage, Router, Masker }
